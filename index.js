6d6f095d1876ac6e49c2f3c362b99c8811439e99
andrey78787878 <andrey.tkach78@gmail.com>
2025-06-15 21:59:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Render использует переменную PORT
 
 // === Настройки ===
 const TELEGRAM_TOKEN = '8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q';
