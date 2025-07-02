1a7f6950f44e4e6b547ae6f855da95f7f7bbb201
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-02 20:13:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,5 @@
-const BOT_TOKEN = "8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q";
-const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
-const TELEGRAM_FILE_API = `https://api.telegram.org/file/bot${BOT_TOKEN}`;
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxlBSrezVPwtPkSSuKQkzoHM0v5lm4mcc5AGBCOIwsu7PzmZ16WN4Rc6KB44PLBuRuG/exec";
-const FOLDER_ID = "1lYjywHLtUgVRhV9dxW0yIhCJtEfl30ClaYSECjrD8ENyh1YDLEYEvbnegKe4_-HK2QlLWzVF";
-const SPREADSHEET_ID = "1u48GTrioEVs_3P3fxcX0e7pKZmYwZyE8HioWJHgRZTc";
-
 module.exports = {
-  BOT_TOKEN,
-  TELEGRAM_API,
-  TELEGRAM_FILE_API,
-  GOOGLE_SCRIPT_URL,
-  FOLDER_ID,
-  SPREADSHEET_ID,
+  BOT_TOKEN: "8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby_gOrwjaB7LgGJcCarpUM8SsyhWzUtMJSN3kddZKm5AToFlWQsErAKxNu9l2UC2JRE/exec",
+  FOLDER_ID: "1lYjywHLtUgVRhV9dxW0yIhCJtEfl30ClaYSECjrD8ENyh1YDLEYEvbnegKe4_-HK2QlLWzVF"
 };
