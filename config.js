fea38ed4da403310c7fb778b0012689ef65b33fc
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-02 21:49:52 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 module.exports = {
   BOT_TOKEN: "8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q",
+
+  TELEGRAM_API: "https://api.telegram.org",
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby_gOrwjaB7LgGJcCarpUM8SsyhWzUtMJSN3kddZKm5AToFlWQsErAKxNu9l2UC2JRE/exec",
-  FOLDER_ID: "1lYjywHLtUgVRhV9dxW0yIhCJtEfl30ClaYSECjrD8ENyh1YDLEYEvbnegKe4_-HK2QlLWzVF"
+  FOLDER_ID: "1lYjywHLtUgVRhV9dxW0yIhCJtEfl30ClaYSECjrD8ENyh1YDLEYEvbnegKe4_-HK2QlLWzVF",
+
+  // Дополнительные настройки (если нужно)
+  GOOGLE_DRIVE_SCOPE: ["https://www.googleapis.com/auth/drive"],
+  PORT: process.env.PORT || 3000
 };
