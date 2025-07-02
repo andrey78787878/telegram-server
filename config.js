411009d11d56df1d9c6fe31197db0d0ac6c0c280
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-02 22:39:04 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Ваш токен бота Telegram (BotFather)
+  BOT_TOKEN: "8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q",
+
+  // URL вашего Google Apps Script (развернутый веб‑приложение)
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby_gOrwjaB7LgGJcCarpUM8SsyhWzUtMJSN3kddZKm5AToFlWQsErAKxNu9l2UC2JRE/exec",
+
+  // ID папки на Google Диске, куда будут загружаться фото
+  FOLDER_ID: "1lYjywHLtUgVRhV9dxW0yIhCJtEfl30ClaYSECjrD8ENyh1YDLEYEvbnegKe4_-HK2QlLWzVF"
+};
