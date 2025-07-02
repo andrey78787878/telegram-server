6496b31623f1618f5e6915d4bdcb864d5d6a1fdf
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-02 22:27:24 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+const BOT_TOKEN = "8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q";
+
 module.exports = {
-  BOT_TOKEN: "8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q",
+  BOT_TOKEN,
   TELEGRAM_API: `https://api.telegram.org/bot${BOT_TOKEN}`,
   TELEGRAM_FILE_API: `https://api.telegram.org/file/bot${BOT_TOKEN}`,
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby_gOrwjaB7LgGJcCarpUM8SsyhWzUtMJSN3kddZKm5AToFlWQsErAKxNu9l2UC2JRE/exec",
