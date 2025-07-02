8319161a5b80bdab11a7e718f5b72834ab58de09
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-03 00:33:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
+const BOT_TOKEN = "8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q";
+
 module.exports = {
-  // Ваш токен бота Telegram (BotFather)
-  BOT_TOKEN: "8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q",
-
-  // URL вашего Google Apps Script (развернутый веб‑приложение)
+  BOT_TOKEN,
+  TELEGRAM_API: `https://api.telegram.org/bot${BOT_TOKEN}`,
+  TELEGRAM_FILE_API: `https://api.telegram.org/file/bot${BOT_TOKEN}`,
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby_gOrwjaB7LgGJcCarpUM8SsyhWzUtMJSN3kddZKm5AToFlWQsErAKxNu9l2UC2JRE/exec",
-
-  // ID папки на Google Диске, куда будут загружаться фото
-  FOLDER_ID: "1lYjywHLtUgVRhV9dxW0yIhCJtEfl30ClaYSECjrD8ENyh1YDLEYEvbnegKe4_-HK2QlLWzVF"
+  FOLDER_ID: "1lYjywHLtUgVRhV9dxW0yIhCJtEfl30ClaYSECjrD8ENyh1YDLEYEvbnegKe4_-HK2QlLWzVF",
+  PORT: process.env.PORT || 3000
 };
