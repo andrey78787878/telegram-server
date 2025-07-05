e9f00374e610e5775f8bbb54bd148a6dee835577
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-05 16:42:26 +0500

    Update uploadTelegramPhotoToDrive.js

--- uploadTelegramPhotoToDrive.js
+++ uploadTelegramPhotoToDrive.js
@@ -3,12 +3,18 @@ const FormData = require('form-data');
 const fs = require('fs');
 const path = require('path');
 
-async function uploadTelegramPhotoToDrive(fileUrl) {
-  const fileName = `photo_${Date.now()}.jpg`;
-  const tempPath = path.join(__dirname, fileName);
+const TELEGRAM_FILE_API = 'https://api.telegram.org/file/bot8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q';
 
+async function uploadPhotoToDrive(file_id, TELEGRAM_FILE_API) {
+  const BOT_TOKEN = '8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q';
+  const getFileUrl = `https://api.telegram.org/bot${BOT_TOKEN}/getFile?file_id=${file_id}`;
+  const res = await axios.get(getFileUrl);
+  const filePath = res.data.result.file_path;
+  const fileUrl = `${TELEGRAM_FILE_API}/${filePath}`;
+
+  const tempPath = path.join(__dirname, 'temp.jpg');
   const writer = fs.createWriteStream(tempPath);
-  const response = await axios({ url: fileUrl, method: 'GET', responseType: 'stream' });
+  const response = await axios.get(fileUrl, { responseType: 'stream' });
   response.data.pipe(writer);
 
   await new Promise((resolve, reject) => {
@@ -16,14 +22,15 @@ async function uploadTelegramPhotoToDrive(fileUrl) {
     writer.on('error', reject);
   });
 
-  const form = new FormData();
-  form.append('file', fs.createReadStream(tempPath));
-  const res = await axios.post('https://script.google.com/macros/s/AKfycbxeXikOhZy-HlXNTh4Dpz7FWqBf1pRi6DWpzGQlFQr8TSV46KUU_-FJF976oQrxpHAx/exec', form, {
-    headers: form.getHeaders(),
+  const formData = new FormData();
+  formData.append('photo', fs.createReadStream(tempPath));
+
+  const uploadRes = await axios.post('https://script.google.com/macros/s/AKfycbxeXikOhZy-HlXNTh4Dpz7FWqBf1pRi6DWpzGQlFQr8TSV46KUU_-FJF976oQrxpHAx/exec', formData, {
+    headers: formData.getHeaders(),
   });
 
   fs.unlinkSync(tempPath);
-  return res.data.url;
+  return uploadRes.data.photoUrl;
 }
 
-module.exports = { uploadTelegramPhotoToDrive };
+module.exports = { uploadPhotoToDrive };
