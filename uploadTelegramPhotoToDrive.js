f5b70a6e5489b69c7f610bda370f4587271ff654
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-07 00:33:21 +0500

    Update uploadTelegramPhotoToDrive.js

--- uploadTelegramPhotoToDrive.js
+++ uploadTelegramPhotoToDrive.js
@@ -3,7 +3,7 @@ const FormData = require('form-data');
 
 const BOT_TOKEN = '8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q';
 const TELEGRAM_FILE_API = `https://api.telegram.org/file/bot${BOT_TOKEN}`;
-const GAS_UPLOAD_URL = 'https://script.google.com/macros/s/AKfycbwnxJm00oGfpCChFrhu0ov3PZ7iVn7i1RTRnyeP8DhXCw2QUHqWRyiyGDU5jvJROY9G/exec';
+const GAS_UPLOAD_URL = 'https://script.google.com/macros/s/AKfycbwYycNWHJanlUL-vDM6KptXod9GdbzcVa6HI67ttSfRkIPkSYuDQdiEzGCDkRHSKkLV/exec';
 
 async function uploadPhotoToDrive(file_id) {
   try {
