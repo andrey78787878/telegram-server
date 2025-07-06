8c89a0c00e8cdc0c6e2e94d468ff921f39e6de69
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-06 20:38:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 const BOT_TOKEN = '8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q';
 const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbyS1vPiaxs488I28pRPcwG_OMVd3eBRX0dqk2tPc8d8HwASxEUXi3mJsps4o-n033-3/exec`;
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyS1vPiaxs488I28pRPcwG_OMVd3eBRX0dqk2tPc8d8HwASxEUXi3mJsps4o-n033-3/exec';
 
 const EXECUTORS = [
   { text: '@EvelinaB87', value: '@EvelinaB87' },
