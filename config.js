7d5a9b296b8a81dc181b55e4dbfc1bc9fa3e3a78
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-07 00:28:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
-const BOT_TOKEN = '8005595415:AAHxAw2UlTYwhSiEcMu5CpTBRT_3-epH12Q';
+require('dotenv').config();
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 module.exports = {
   BOT_TOKEN,
   TELEGRAM_API: `https://api.telegram.org/bot${BOT_TOKEN}`,
   TELEGRAM_FILE_API: `https://api.telegram.org/file/bot${BOT_TOKEN}`,
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwnxJm00oGfpCChFrhu0ov3PZ7iVn7i1RTRnyeP8DhXCw2QUHqWRyiyGDU5jvJROY9G/exec'
+  GAS_URL: process.env.GAS_URL,
+  PORT: process.env.PORT || 3000
 };
