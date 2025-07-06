4587a5707c7075a88d0e88809214cac3618aeda5
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-06 21:35:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   BOT_TOKEN,
   TELEGRAM_API: `https://api.telegram.org/bot${BOT_TOKEN}`,
   TELEGRAM_FILE_API: `https://api.telegram.org/file/bot${BOT_TOKEN}`,
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbyS1vPiaxs488I28pRPcwG_OMVd3eBRX0dqk2tPc8d8HwASxEUXi3mJsps4o-n033-3/exec'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbx-yVE9Z8lWDVNUoLrGbuEfp7hyvHogQfPLc9ehH6afPmAEIlqLSj6r3RuzTK9NmA4W/exec'
 };
