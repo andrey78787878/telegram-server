525778b5a7e16c1a71e38efb55564bc92709f9a8
andrey78787878 <andrey.tkach78@gmail.com>
2025-07-13 15:52:48 +0500

    server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.json());
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const TELEGRAM_API = https://api.telegram.org/bot${BOT_TOKEN};
-const TELEGRAM_FILE_API = https://api.telegram.org/file/bot${BOT_TOKEN};
+const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
+const TELEGRAM_FILE_API = `https://api.telegram.org/file/bot${BOT_TOKEN}`;
 const GAS_WEB_APP_URL = process.env.GAS_WEB_APP_URL;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 const PORT = process.env.PORT || 3000;
